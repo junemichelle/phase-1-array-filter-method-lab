44230ef880e7c98d3304ff6c7334e775e6de381b
michelle <junemichelle20@gmail.com>
2023-09-27 23:45:39 +0300

    completed assignments

--- index.js
+++ index.js
@@ -2,38 +2,18 @@
 
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 function findMatching(drivers,name){
-  const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  const matchingDrivers = drivers.filter(function(driver) {return driver.toLowerCase() === name.toLowerCase()});
   return matchingDrivers;
 }
 
 function fuzzyMatch(drivers,name){
-  const matchingLetters=drivers.filter(driver=>driver.startsWith(name))
+  const matchingLetters=drivers.filter(function(driver){return driver.startsWith(name)});
   return matchingLetters;
 }
 
 function matchName(drivers,name){
 
-  const matchingName=drivers.filter(driver => driver.name === name);
+  const matchingName=drivers.filter(function(driver){return driver.name === name});
   return matchingName;
 }
-  // const matchingNames=drivers.filter(driver=>drivers.name===name)
-  // return matchingNames;
-
-  // const drivers = [
-  //   {
-  //     name: 'Bobby',
-  //     hometown: 'Pittsburgh' },
-  //   {
-  //     name: 'Sammy',
-  //     hometown: 'New York' } ,
-  //   {
-  //     name: 'Sally',
-  //     hometown: 'Cleveland' },
-  //   {
-  //     name: 'Annette',
-  //     hometown: 'Los Angeles' },
-  //   {
-  //     name: 'Bobby',
-  //     hometown: 'Tampa Bay' }
-  // ];
-
+  
